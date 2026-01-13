3495cfc649df379a6ef477eb11d44d36dc82e6a7
Srushti <srushtiw13020@gmail.com>
2026-01-13 20:52:32 +0530

    changes done

--- database.py
+++ database.py
@@ -1,6 +1,19 @@
 import sqlite3
+from flask import g
+
+DATABASE = "complaint.db"
 
 def get_db():
-    conn = sqlite3.connect("complaint.db")
-    conn.row_factory = sqlite3.Row
-    return conn
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            DATABASE,
+            timeout=30,
+            check_same_thread=False
+        )
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
